47a29afbf8dc9a82801000935e0dd3a2421ac984
ZGooOO <979220263@qq.com>
2021-06-15 11:24:51 +0800

    Update msfcnet_resnet.py

--- src/lib/models/networks/msfcnet_resnet.py
+++ src/lib/models/networks/msfcnet_resnet.py
@@ -1,6 +1,3 @@
-##+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-##  ResNeSt_Feature Align: by Tong Zhang
-##+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 """ResNet variants"""
 from __future__ import absolute_import
 from __future__ import division

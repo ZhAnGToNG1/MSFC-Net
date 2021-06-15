afd2589642f2fe1f8aba4bc9dd63c09acce6b463
ZGooOO <979220263@qq.com>
2021-06-15 12:25:47 +0800

    Update DOTA.py

--- src/lib/datasets/dataset/DOTA.py
+++ src/lib/datasets/dataset/DOTA.py
@@ -3,7 +3,6 @@ from __future__ import division
 from __future__ import print_function
 
 import pycocotools.coco as coco
-from pycocotools.cocoeval import COCOeval
 import numpy as np
 import json
 import os
